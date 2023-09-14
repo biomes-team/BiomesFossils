2f2cf122f9129bb34e09c36a70765849a6d804f6
Zylleon <56230821+Zylleon@users.noreply.github.com>
2023-09-13 18:00:19 -0600

    Large fossils must be placed on display bases

---
+++ 1.4/Source/BMT_Fossils/PlaceWorker_OnDisplayBaseLarge.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace BMT_Fossils
+{
+    public class PlaceWorker_OnDisplayBaseLarge : PlaceWorker
+    {
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+        {
+            Thing thing2 = map.thingGrid.ThingAt(loc, FossilsDefOf.BMT_DisplayLarge);
+            if (thing2 == null || thing2.Position != loc)
+            {
+                return "BMT_PlaceOnDisplay".Translate();
+            }
+            return true;
+        }
+
+
+        public override bool ForceAllowPlaceOver(BuildableDef otherDef)
+        {
+            return otherDef == FossilsDefOf.BMT_DisplayLarge;
+        }
+    }
+}
