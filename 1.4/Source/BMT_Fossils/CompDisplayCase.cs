c7d4e85cbcc1d06428547b29b8432f5df2ab6ca6
Zylleon <56230821+Zylleon@users.noreply.github.com>
2023-10-06 06:14:08 -0600

    display bases

---
+++ 1.4/Source/BMT_Fossils/CompDisplayCase.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace BMT_Fossils
+{
+	[StaticConstructorOnStartup]
+	public class CompDisplayCase : ThingComp
+    {
+		public CompProperties_DisplayCase Props => (CompProperties_DisplayCase)props;
+
+		public override void PostDraw()
+		{
+			Log.Message("Houston this is POST DRAW");
+			base.PostDraw();
+			//if (((Building_Storage)parent).GetSlotGroup().HeldThings.Count() > 0)
+			//{
+				Vector3 drawPos = parent.DrawPos;
+				drawPos.y += 3f / 74f;
+
+				Props.topGraphicData.Graphic.Draw(drawPos, Rot4.North, parent);
+            
+			//}
+		}
+
+	}
+}
---
+++ 1.4/Source/BMT_Fossils/CompProperties_DisplayCase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace BMT_Fossils
+{
+    public class CompProperties_DisplayCase : CompProperties
+    {
+        public GraphicData topGraphicData;
+
+        public CompProperties_DisplayCase()
+        {
+            compClass = typeof(CompDisplayCase);
+        }
+
+        public override void DrawGhost(IntVec3 center, Rot4 rot, ThingDef thingDef, Color ghostCol, AltitudeLayer drawAltitude, Thing thing = null)
+        {
+            GhostUtility.GhostGraphicFor(topGraphicData.Graphic, thingDef, ghostCol).DrawFromDef(center.ToVector3ShiftedWithAltitude(drawAltitude), rot, thingDef);
+        }
+    }
+}
