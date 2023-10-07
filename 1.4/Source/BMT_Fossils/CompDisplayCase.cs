25ded072dc701cc36981be9f24e05edc8d3538d5
Zylleon <56230821+Zylleon@users.noreply.github.com>
2023-10-07 10:31:33 -0600

    display cases working, centered small skeletons

--- 1.4/Source/BMT_Fossils/CompDisplayCase.cs
+++ 1.4/Source/BMT_Fossils/CompDisplayCase.cs
@@ -18,15 +18,16 @@ namespace BMT_Fossils
 		{
 			Log.Message("Houston this is POST DRAW");
 			base.PostDraw();
-			//if (((Building_Storage)parent).GetSlotGroup().HeldThings.Count() > 0)
-			//{
-				Vector3 drawPos = parent.DrawPos;
-				drawPos.y += 3f / 74f;
+            if (((Building_Storage)parent).GetSlotGroup().HeldThings.Count() > 0)
+            {
+                Vector3 drawPos = parent.DrawPos;
 
-				Props.topGraphicData.Graphic.Draw(drawPos, Rot4.North, parent);
-            
-			//}
-		}
+
+                //Props.topGraphicData.Graphic.Draw(drawPos, Rot4.North, parent);
+                Props.topGraphicData.Graphic.Draw(GenThing.TrueCenter(parent.Position, parent.Rotation, parent.def.size, Altitudes.AltitudeFor(AltitudeLayer.MoteOverhead)), parent.Rotation, parent);
+
+            }
+    }
 
 	}
 }
